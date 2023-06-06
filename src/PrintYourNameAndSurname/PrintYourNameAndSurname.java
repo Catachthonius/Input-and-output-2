e974e38d690271e3c539e76f244f7a186aaf40bc
Antonio Antinoro <catachthonius01@gmail.com>
2023-06-06 18:49:44 +0200

    First commit

---
+++ src/PrintYourNameAndSurname/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package PrintYourNameAndSurname;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Antonio ");
+        System.out.print("Antinoro");
+    }
+}
